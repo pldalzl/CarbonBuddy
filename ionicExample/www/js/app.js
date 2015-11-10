75f9169b985681b839cd62aa81120f2684ce22eb
pldalzl <pldalzl@gmail.com>
2015-11-10 14:27:47 +0000

    add map and firebase connection

--- ionicExample/www/js/app.js
+++ ionicExample/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers','starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -98,4 +98,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/sign-in');
 
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('map', {
+      url: '/map',
+      templateUrl: 'templates/map.html',
+      controller: 'MapCtrl'
+    })
+
+  $urlRouterProvider.otherwise('/map');
 });
+
--- ionicExample/www/js/controllers.js
+++ ionicExample/www/js/controllers.js
@@ -9,7 +9,7 @@ angular.module('starter.controllers', ['ionic'])
 
   })
 
-.controller('DashCtrl', function($scope) {})
+  .controller('DashCtrl', function($scope) {})
 
   .controller('ChatsCtrl', function($scope, Chats) {
     // With the new view caching in Ionic, Controllers are only called
@@ -37,11 +37,75 @@ angular.module('starter.controllers', ['ionic'])
     $scope.chat = Chats.get($stateParams.chatId);
   })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+  .controller('AccountCtrl', function($scope) {
+    $scope.settings = {
+      enableFriends: true
+    };
+  })
+
+angular.module('starter.controllers', ['ionic','firebase'])
+
+  .controller('MapCtrl', ['$scope','$firebase','$ionicPopup', function($scope,$firebase,$ionicPopup){
+    $scope.saveDetails = function(){
+      var lat = $scope.user.latitude;
+      var lgt = $scope.user.longitude;
+      var des = $scope.user.desc;
+
+      // Code to write to Firebase will be here
+      var firebaseObj = new Firebase("https://torrid-heat-7043.firebaseio.com/MapDetails");
+      var fb = $firebase(firebaseObj);
+
+      fb.$push({
+        latitude: lat,
+        longitude: lgt,
+        description: des
+      }).then(function(ref) {
+        $scope.user = {};
+        $scope.showAlert();
+      }, function(error) {
+        console.log("Error:", error);
+      });
+    }
+    $scope.showAlert = function() {
+      $ionicPopup.alert({
+        title: 'iMapApp',
+        template: 'Your location has been saved!!'
+      });
+    };
+// Code will be here
+
+  }])
+
+  .directive('map', function() {
+    return {
+      restrict: 'A',
+      link:function(scope, element, attrs){
+
+        var zValue = scope.$eval(attrs.zoom);
+        var lat = scope.$eval(attrs.lat);
+        var lng = scope.$eval(attrs.lng);
+
+
+        var myLatlng = new google.maps.LatLng(lat,lng),
+          mapOptions = {
+            zoom: zValue,
+            center: myLatlng
+          },
+          map = new google.maps.Map(element[0],mapOptions),
+          marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            draggable:true
+          })
+        google.maps.event.addListener(marker, 'dragend', function(evt){
+          scope.$parent.user.latitude = evt.latLng.lat();
+          scope.$parent.user.longitude = evt.latLng.lng();
+          scope.$apply();
+        });
+
+      }
+    };
+  });
 
 
 
